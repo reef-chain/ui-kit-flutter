323f3a7f6a681b85f0f1d3e557b046f813dd54c0
Anukul Pandey <anukul.030601@gmail.com>
2024-04-02 15:36:27 +0530

    feat: gradient button

---
+++ lib/reef_ui_kit.dart
@@ -0,0 +1,4 @@
+library reef_ui_kit;
+
+export 'package:reef_ui_kit/src/GradientButton.dart';
+export 'package:reef_ui_kit/constants/Styles.dart';
---
+++ lib/src/GradientButton.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:reef_ui_kit/constants/Styles.dart';
+
+class GradientButton extends StatelessWidget {
+  final String title;
+  final VoidCallback? onPressed;
+  final double width;
+  const GradientButton({required this.title, required this.onPressed,this.width=double.infinity, Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: width,
+      child: ElevatedButton(
+          style: ElevatedButton.styleFrom(
+            shape:
+                RoundedRectangleBorder(borderRadius: BorderRadius.circular(14)),
+            shadowColor: const Color(0x559d6cff),
+            elevation: 0,
+            backgroundColor: const Color(0xffe6e2f1),
+            padding: const EdgeInsets.all(0),
+          ),
+          onPressed: () => {
+                //todo: anukulpandey
+              },
+          child: Ink(
+            width: double.infinity,
+            padding: const EdgeInsets.symmetric(vertical: 15, horizontal: 22),
+            decoration: BoxDecoration(
+              color: const Color(0xffe6e2f1),
+              gradient: Styles.buttonGradient,
+              borderRadius: const BorderRadius.all(Radius.circular(14.0)),
+            ),
+            child: Center(
+              child: Text(
+                title,
+                style: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.w700,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          )),
+    );
+  }
+}
